9034466e71bc34c57d97b1f55945d361553afe37
Jhonathanas <jonathanadisaputro99@students.amikom.ac.id>
2024-02-11 00:21:41 +0700

    Memperbaiki button demo

--- src/components/portofolio/portofolio.jsx
+++ src/components/portofolio/portofolio.jsx
@@ -6,24 +6,28 @@ const item=[
         id:1,
         title:"Jo Movie",
         img:'/public/port1.png',
+        demo: `https://jomovie.netlify.app/`,
         desc:'Pencarian informasiC judul film dan informasi yang lainnya yang diambil mengunakan api database dari TDM lalu ditampilkan kedepan dengan framwork Reack'
     },
     {
         id:2,
         title:"Next.js Commerce",
         img:"https://images.pexels.com/photos/18931202/pexels-photo-18931202/free-photo-of-a-white-building-with-trees-in-front-of-it.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load",
+        demo: '',
         desc:'sadfnaskdlfansdfj sdajfhnka nasdjfhruffifruui sdckmmcmkcv papapas sdkfjfi jaskf'
     },
     {
         id:3,
         title:"Laravel Commerce",
         img:"https://images.pexels.com/photos/18931202/pexels-photo-18931202/free-photo-of-a-white-building-with-trees-in-front-of-it.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load",
+        demo: '',
         desc:'sadfnaskdlfansdfj sdajfhnka nasdjfhruffifruui sdckmmcmkcv papapas sdkfjfi jaskf'
     },
     {
         id:4,
         title:"Music Commerce",
         img:"https://images.pexels.com/photos/18931202/pexels-photo-18931202/free-photo-of-a-white-building-with-trees-in-front-of-it.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load",
+        demo: '',
         desc:'sadfnaskdlfansdfj sdajfhnka nasdjfhruffifruui sdckmmcmkcv papapas sdkfjfi jaskf'
     }
 ];
@@ -34,7 +38,9 @@ const Single = ({ item }) => {
         target: ref,
         // offset:['start start', 'end start']
     });
-
+    const redirectToDemo = (demoUrl) => {
+        window.location.href = demoUrl;
+    }        
     const y = useTransform(scrollYProgress, [0, 1], [-200,200])
 
     return <section>
@@ -46,8 +52,8 @@ const Single = ({ item }) => {
                 <motion.div className='textContainer' style={{ y }}>
                     <h2>{item.title}</h2>
                     <p>{item.desc}</p>
-                    <button>See Demo</button>
-                </motion.div>
+                    <button onClick={() => redirectToDemo(item.demo)}>See Demo</button>
+                    </motion.div>
             </div>
         </div>
         </section>
